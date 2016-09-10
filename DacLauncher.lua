ba49f566ffa9b809d8de8191bf348495f9d41625
TheDacinator <dacman9@gmail.com>
2016-09-09 20:59:54 -0400

    Update DacLauncher.lua

--- DacLauncher.lua
+++ DacLauncher.lua
@@ -1,5 +1,9 @@
 if not http then
   error("Http api not enabled!")
 end
-local file = http.get("https://raw.githubusercontent.com/TheDacinator/DacLauncher/master/DacLauncher2")
-loadstring(file.readAll())()
+local page = http.get("https://raw.githubusercontent.com/TheDacinator/DacLauncher/master/DacLauncher2")
+local file = fs.open("DacLauncherDontRun,"w")
+file.write(page.readAll())
+file.close()
+page.close()
+shell.run("DacLauncherDontRun")
